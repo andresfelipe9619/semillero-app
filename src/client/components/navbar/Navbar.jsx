9ecbf4c1a331f03fa979d211478209055219c41f
Andres Suarez <andresfelipe9619@gmail.com>
2022-07-04 20:50:54 -0500

    :lipstick: Build navbar

---
+++ src/client/components/navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Box, Typography, Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { Search, StyledInputBase, SearchIconWrapper } from './styles';
+
+export default function Navbar({ loading, handleSearch, handleRegister }) {
+  const [documentToSearch, setDocumentToSearch] = useState('');
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setDocumentToSearch(value);
+  }
+  return (
+    <Box sx={{ flexGrow: 1 }} bgcolor={'white'}>
+      <AppBar position="static" color="inherit">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            color="primary"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              fontWeight: 'bold',
+              display: { xs: 'none', sm: 'block' },
+            }}
+          >
+            Semillero Universidad del Valle
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              onChange={handleChange}
+              placeholder="Búscar estudiante..."
+              inputProps={{ 'aria-label': 'buscar estudiante' }}
+            />
+          </Search>
+          <Button
+            disabled={loading}
+            variant="contained"
+            sx={{ margin: '0px 8px' }}
+            onClick={() => handleSearch(documentToSearch)}
+          >
+            Buscar
+          </Button>
+          <Button
+            disabled={loading}
+            variant="outlined"
+            sx={{ margin: '0px 8px' }}
+            onClick={() => handleRegister(documentToSearch)}
+          >
+            Registrar
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
