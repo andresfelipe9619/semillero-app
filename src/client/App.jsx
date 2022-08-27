1d368b95ba242b4786dc395613f073e2e2e89542
Andres Suarez <andresfelipe9619@gmail.com>
2022-08-27 09:07:14 -0500

    :wrench: Use spanish as default language

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import esLocale from 'date-fns/locale/es';
 import GlobalState from './context/GlobalState';
 import AppRouter from './AppRouter';
 import theme from './theme';
@@ -9,7 +10,10 @@ import theme from './theme';
 const App = () => (
   <GlobalState>
     <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        adapterLocale={esLocale}
+      >
         <AppRouter />
       </LocalizationProvider>
     </ThemeProvider>
